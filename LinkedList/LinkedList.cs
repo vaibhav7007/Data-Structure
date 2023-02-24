5df884225b670d37c078f072f6a59fff92538879
vaibhav <vaibhavmehta524@gmail.com>
2023-02-25 01:07:14 +0530

    Create Linked List program

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} insterted into linked List: ", node.data);
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to data structure programs");
+            LinkedList linkedList = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Please enter an option");
+                Console.WriteLine("1:Create linked list");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter right option");
+                        break;
+                }
+            }
+        }
+    }
+}
